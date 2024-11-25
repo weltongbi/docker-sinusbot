7aa4752bda4e4e645be963c68625ecd87779268e
welton da silva castro <weltongbi@gmail.com>
2024-11-24 21:24:29 -0300

    Adicionado o reset password na variavel de ambiente

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo ' __          __  _ _                   _            ' 
+echo ' \ \        / / | | |                 | |           ' 
+echo '  \ \  /\  / /__| | |_ ___  _ __    __| | _____   __' 
+echo '   \ \/  \/ / _ \ | __/ _ \| '\''_ \  / _` |/ _ \ \ / /' 
+echo '    \  /\  /  __/ | || (_) | | | || (_| |  __/\ V /'
+echo '     \/  \/ \___|_|\__\___/|_| |_(_)__,_|\___| \_/'
+
+echo '---------------------------------------------------------'
+
 echo "Start entrypoint"
 sleep 1
 
@@ -7,9 +16,15 @@ echo "Correcting mount point permissions"
 chown -fR "$SINUS_USER":"$SINUS_GROUP" -R "$SINUS_DIR"
 sleep 2
 
-echo "Checking for youtube-dl updates"
-$YTDL_BIN -U
+echo "Checking for yt-dlp updates"
+$YTDLP_BIN -U
 sleep 2
 
-echo "Start sinusbot"
-sudo -u "$SINUS_USER" -g "$SINUS_GROUP" "$SINUS_DIR/sinusbot"
+echo "Checking for server reset password in OVERRIDE_PASSWORD environment variable"
+if [ -n "$OVERRIDE_PASSWORD" ]; then
+    echo "Server reset password found, resetting password"
+    sudo -u "$SINUS_USER" -g "$SINUS_GROUP" "$SINUS_DIR/sinusbot" --override-password="$OVERRIDE_PASSWORD"
+else
+    echo "starting sinusbot"
+    sudo -u "$SINUS_USER" -g "$SINUS_GROUP" "$SINUS_DIR/sinusbot"
+fi
